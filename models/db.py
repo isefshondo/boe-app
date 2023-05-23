9033daa5c165a061e7f0e423fc0f498847aa9eaa
Isabelle Hondo <isefshondo@gmail.com>
2023-05-23 09:49:41 -0300

    Feat: Organizing folder

--- models/db.py
+++ models/db.py
@@ -1,5 +1,13 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-client = MongoClient('mongodb://localhost:27017')
+import os
 
-db = client['boe-app']
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_password = os.getenv("DB_PASSWORD")
+
+client = MongoClient("mongodb+srv://" + db_host + ":" + db_password + "@cluster0.oxt2a9r.mongodb.net/?retryWrites=true&w=majority")
+
+db = client["boe"]
