1aebabd977abf7be0dabd1ec15c3efc410f11a67
Isabelle Hondo <isefshondo@gmail.com>
2023-11-23 07:24:37 -0300

    fixing for final presentation

--- models/db.py
+++ models/db.py
@@ -1,21 +1,5 @@
-from pymongo.mongo_client import MongoClient
-from dotenv import load_dotenv
-import os
+from pymongo import MongoClient
 
-load_dotenv()
+client = MongoClient('mongodb://localhost:27017')
 
-mongodb_password = os.getenv("MONGODB_PASSWORD")
-mongodb_name = os.getenv("MONGODB_NAME")
-
-uri = f"mongodb+srv://isefshondo:{mongodb_password}@boecluster.bz4yg1l.mongodb.net/{mongodb_name}?retryWrites=true&w=majority"
-
-# Create a new client and connect to the server
-client = MongoClient(uri)
-db = client[f"{mongodb_name}"]
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+db = client['boe-app']
